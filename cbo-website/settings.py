54504784d190fed54757d0bd4a319708e0ffdad3
Ugo Bindini <ugobindini@yahoo.it>
2025-04-01 19:08:42 +0200

    Minor fixes

--- cbo-website/settings.py
+++ cbo-website/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, "static") # uncomment for local servering
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # uncomment for pythonanywhere
 STATIC_URL = 'staticfiles/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"), ) # uncomment for local servering
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"), ) # uncomment for local servering
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
