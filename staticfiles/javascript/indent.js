ba818b93aea4d2820f4fc8a24323363c8287cbfc
Ugo Bindini <ugobindini@yahoo.it>
2025-04-04 13:09:39 +0200

    Added metric CB227

--- staticfiles/javascript/indent.js
+++ staticfiles/javascript/indent.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".verse").each(function() {
     var indent = $(this).data("indent");
-    $(this).children(".verse-text").css("padding-left", "calc(" + indent + " * 20px");
+    $(this).children(".verse-text").css("margin-left", "calc(" + indent + " * 20px");
   });
 });
